74a41ba9ba54991d85f91aa549e1536c0fe6c5b1
mellis <mellis.eriecon@gmail.com>
2021-11-27 22:07:45 -0500

    finished 3.1.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("this is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("this is a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("robot name is " + playerName);
+var name = "mike";
+console.log(name);
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
