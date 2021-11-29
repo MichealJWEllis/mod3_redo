b5715a9d29bc9a01ca9ef76c70a6d7d8cf9d84ad
Micheal Ellis <mellis@erieconstruction.net>
2021-11-29 13:20:03 -0500

    finished 3.2.6

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,46 @@
+// Game states
+// "WIN" - Player robot has defeated all enemy-robots
+//  * Fight all enemy robos
+// * Defeat each enemy robo
+// "LOSE" - Player robo's health is zero or less
+
 window.alert("Welcome to Robot Gladiators!");
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+// console.log(playerName, playerAttack, playerHealth);
+// console.log(enemyNames.length);
+
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+
+// for(var i = 0; i < enemyNames.length; i++){
+//     console.log(" ");
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + " index");
+// }
+// Example of looping through an array! 
+// for(var i = 0; i < enemyNames.length; i++){
+//     console.log(enemyNames[i] , i);
+// }
+// for(var i = 0; i < 3; i++){
+//     console.log("apple",  i);
+// }
+// for([initial expression]; [condition]; [increment expression]) {
+//     statement
+//  }
+
+
+
+var fight = function (enemyName) {
     // if player choses to fight, then fight
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     if(promptFight === "fight" || promptFight === "FIGHT"){
@@ -62,4 +92,7 @@ var fight = function () {
 
 };
 
-fight();
+// fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+};
